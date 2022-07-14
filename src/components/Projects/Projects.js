879ec2a9b22aa4e141ce265912282f62e0e013cb
vino00619 <vinoth00619@gmail.com>
2022-07-14 21:02:58 +0530

    completed portfolio

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,75 @@
+import React from 'react';
+
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { projects } from '../../constants/constants';
+
+const projectsDemo = [{
+  title: 'Project 1',
+  description: 'This is a brief explanation about the project',
+  tags: ['mongoDB', 'Express', 'React', 'Node'],
+  source: "",
+  visit: "",
+  image: "https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg",
+  id: 0
+},
+{
+  title: 'Project 2',
+  description: 'This is a brief explanation about the project',
+  tags: ['mongoDB', 'Express', 'React', 'Node'],
+  source: "",
+  visit: "",
+  image: "https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg",
+  id: 1
+},
+{
+  title: 'Project 3',
+  description: 'This is a brief explanation about the project',
+  tags: ['mongoDB', 'Express', 'React', 'Node'],
+  source: "",
+  visit: "",
+  image: "https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg",
+  id: 2
+},
+{
+  title: 'Project 4',
+  description: 'This is a brief explanation about the project',
+  tags: ['mongoDB', 'Express', 'React', 'Node'],
+  source: "",
+  visit: "",
+  image: "https://s3-ap-south-1.amazonaws.com/static.awfis.com/wp-content/uploads/2017/07/07184649/ProjectManagement.jpg",
+  id: 3
+}];
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projectsDemo.map(({id, image, title, description, tags, source, visit})=> (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
---
+++ src/components/Technologies/Technologies.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
+import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+
+const Technologies = () =>  (
+  <Section id="tech">
+    <SectionDivider /> <br /> <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've hands-on experience with a wide range of technologies in the web development world.
+      From Back-end to Front-end.
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            React.js <br />
+            HTML <br />
+            CSS 
+          </ListParagraph>
+        </ListContainer>
+      </ListItem><ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End</ListTitle>
+          <ListParagraph>
+            Node.js <br />
+            Express.js <br />
+            MongoDB
+          </ListParagraph>
+        </ListContainer>
+      </ListItem><ListItem>
+        <DiZend size="3rem" />
+        <ListContainer>
+          <ListTitle>Tools</ListTitle>
+          <ListParagraph>
+            VS code<br />
+            Postman<br />
+            Mongo DB Atlas<br />
+            Figma
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
+);
+
+export default Technologies;
