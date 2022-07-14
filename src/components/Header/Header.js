c59bbdde1f59c1cefe8ff58bc450909798013a1e
vino00619 <vinoth00619@gmail.com>
2022-07-14 23:51:57 +0530

    added a small change

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () =>  (
     <Div1>
       <Link href="/">
         <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }} >
-          <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+          <DiCssdeck size="3rem" style={{marginRight:"7px"}} /><Span>Vinoth's Portfolio</Span>
         </a>
       </Link>
     </Div1>
