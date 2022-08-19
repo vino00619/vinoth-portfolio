21d536e2f1dfc2b930dd8d442d8bbb4b48a57bcd
Vinoth Manoharan <39054777+vino00619@users.noreply.github.com>
2022-08-19 18:51:19 +0530

    Update Footer.js
    adding social media url's

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -22,13 +22,13 @@ const Footer = () => {
           <Slogan>Innovating one project at a time</Slogan>
         </CompanyContainer>
         <SocialContainer>
-          <SocialIcons href="https://github.com">
+          <SocialIcons href="https://github.com/vino00619">
           <AiFillGithub size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://linkedin.com">
+          <SocialIcons href="https://www.linkedin.com/in/vinoth-manoharan-7437b1a3">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://instagram.com">
+          <SocialIcons href="https://www.instagram.com/vino_codes/">
             <AiFillInstagram size="3rem" />
           </SocialIcons>
         </SocialContainer>
