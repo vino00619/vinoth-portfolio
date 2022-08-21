d2aec48fad1d208e6b54a5835b66f294ec3cd13b
Vinoth Manoharan <39054777+vino00619@users.noreply.github.com>
2022-08-22 01:44:19 +0530

    Update Acomplishments.js

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -1,27 +1,25 @@
-// import React from 'react';
+import React from 'react';
 
-// import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
-// import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
-// const data = [
-//   { number: 20, text: 'Open Source Projects'},
-//   { number: 1000, text: 'Students', },
-//   { number: 1900, text: 'Github Followers', },
-//   { number: 5000, text: 'Github Stars', }
-// ];
+const data = [
+  { text: 'completed and earned a certificate in Full Stack Webdevelopment in Udemy.'},
+  { text: 'hands on experience in WebGIS applications.', },
+];
 
-// const Acomplishments = () => (
-//   <Section>
-//     <SectionTitle>Personal Acomplishments</SectionTitle>
-//     <Boxes>
-//       {data.map((card, index) => (
-//         <Box key={index}>
-//           <BoxNum>{card.number}+</BoxNum>
-//           <BoxText>{card.text}</BoxText>
-//         </Box>
-//       ))}
-//     </Boxes>
-//   </Section>
-// );
+const Acomplishments = () => (
+  <Section>
+    <SectionTitle>Personal Acomplishments</SectionTitle>
+    <Boxes>
+      {data.map((card, index) => (
+        <Box key={index}>
+          //<BoxNum>{card.number}+</BoxNum>
+          <BoxText>{card.text}</BoxText>
+        </Box>
+      ))}
+    </Boxes>
+  </Section>
+);
 
-//export default Acomplishments;
+export default Acomplishments;
