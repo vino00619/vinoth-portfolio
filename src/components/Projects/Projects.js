db8f18f5c7ab526fcd2192bd55379afc9c1fc9c7
Vinoth Manoharan <39054777+vino00619@users.noreply.github.com>
2022-08-22 00:50:56 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-import * as url from '../../../public/images/Travel_Stories.PNG';
+import Travel_Stories from '../../../public/images/Travel_Stories.PNG';
 
 const projectsDemo = [{
   title: 'Travel Stories',
