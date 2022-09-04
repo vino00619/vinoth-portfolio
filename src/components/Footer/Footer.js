29f0224fc62de9a35b54262d6b5cba6b2b3ce0c1
Vinoth Manoharan <39054777+vino00619@users.noreply.github.com>
2022-09-04 22:27:14 +0530

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -28,9 +28,6 @@ const Footer = () => {
           <SocialIcons href="https://www.linkedin.com/in/vinoth-manoharan-7437b1a3">
             <AiFillLinkedin size="3rem" />
           </SocialIcons>
-          <SocialIcons href="https://www.instagram.com/vino_codes/">
-            <AiFillInstagram size="3rem" />
-          </SocialIcons>
         </SocialContainer>
       </SocialIconsContainer>
     </FooterWrapper>
